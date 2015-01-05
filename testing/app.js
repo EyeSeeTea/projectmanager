86aada57bf35ef4298eb7a3e118ac1ff442d1ef1
HelderCastrillon <helder_947@hotmail.com>
2015-01-05 09:29:11 -0500

    autocomplete para las unidades organizativas

--- testing/app.js
+++ testing/app.js
@@ -1,4 +1,4 @@
-var Dhis2Api = angular.module("Dhis2Api", ['ngResource']);
+var Dhis2Api = angular.module("Dhis2Api", ['ngResource', 'ui.bootstrap']);
 
 //Create all common variables of the apps 
 Dhis2Api.factory("commonvariable", function () {
@@ -11,7 +11,12 @@ Dhis2Api.factory("commonvariable", function () {
      return Api; 
 });
 Dhis2Api.factory("GetOrganisationunit",['$resource','commonvariable', function ($resource,commonvariable) {
-	console.log("Aqui >>>"+commonvariable.url);
+	return $resource(commonvariable.url+"organisationUnits", 
+   {}, 
+  { get: { method: "GET"} });
+}]);
+
+Dhis2Api.factory("findOrganisationunitbyName",['$resource','commonvariable', function ($resource,commonvariable) {
 	return $resource(commonvariable.url+"organisationUnits", 
    {}, 
   { get: { method: "GET"} });
@@ -27,5 +32,25 @@ $scope.selectOu = function(ouselected){
 }
 });
 
+Dhis2Api.controller('TypeaheadCtrl', ['$scope','$http','commonvariable','findOrganisationunitbyName',function($scope,$http,commonvariable, findOrganisationunitbyName) {
 
+ 
+	/*$scope.findOrganisationunit1 = function(val) {
+			  return findOrganisationunitbyName.get().then(function(response){
+	      return response.organisationUnits;
+	    });
+	  };
+	  */
+	  // 
+	  $scope.findOrganisationunit = function(val) {
+			  return $http.get(commonvariable.url+'organisationUnits', {
+	      params: {
+	        filter:"name:like:"+val,
+	        fields:"name,id,level,parent"
+	      }
+	    }).then(function(response){
+	      return response.data.organisationUnits;
+	    });
+	  };
 
+}]);
