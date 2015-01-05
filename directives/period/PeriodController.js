e71929644c8dddbe7587ee444e32724fb96515f8
mgarcia <markodavidgm@gmail.com>
2015-01-05 13:19:34 -0500

    Agregar las directivas de periodos

--- directives/period/PeriodController.js
+++ directives/period/PeriodController.js
@@ -1,13 +1,52 @@
-Dhis2Api.directive('d2DrowdownPeriod', function(){
+Dhis2Api.directive('d2Dropdownperiod', function(){
 	return{
 		restrict: 'E',
-		templateUrl: 'directives/period/PeriodView.html'
+		templateUrl: 'directives/period/periodView.html'
 	}
-	}); 
-Dhis2Api.controller("2DrowdownPeriodController", ['$scope','$http', 'GetPeriod',function ($scope, $http,GetPeriod) {
-		$scope.ListPeriod = GetPeriod.get();
-		$scope.selectPeriod = function(PeriodSelected){
-			$scope.periodName=PeriodSelected.name;
-			$scope.periodUid=PeriodSelected.id;
+	});
+
+Dhis2Api.controller("d2DropDownPeriodController", ['$scope',function ($scope) {
+
+	$scope.TipoPeriodo= [
+		{name:"Diario",value:1},
+		{name:"Semanal",value:2},
+		{name:"Mensual",value:3},
+		{name:"Mensual",value:3}
+
+	];
+
+
+		$scope.Meses= [
+			{name:"Enero",value:1},
+			{name:"Febrero",value:2},
+			{name:"Marzo",value:3}
+		];
+
+
+/*	$scope.Anios = function(start, end) {
+		start=1900;
+		end=2015;
+		var result = [];
+		for (var i = start; i <= end; i++) {
+			result.push(i);
 		}
+		return result;
+	};*/
+	var Anios=[];
+	$scope.Anios=[];
+	for (var i= 1900; i<=2020; i++){
+		Anios.push(i);
+	}
+
+
+	$scope.MesSeleccionado = function(peSelected){
+		$scope.monthname=peSelected.name;
+}
+
+//	$scope.AnoSeleccionado = function(anSelected){
+//		$scope.ano=anSelected;
+//	}
+
+
+
 }]);
