d5f93a93edbe2beb70070a4f48976ff9f3ad7596
Adrian Quintana <adrian.perez@ucl.ac.uk>
2015-01-14 19:23:55 +0000

    Añadida progress bar en metadata export

--- modules/metadataexport/metadataexportController.js
+++ modules/metadataexport/metadataexportController.js
@@ -1,11 +1,18 @@
 appManagerMSF.controller('metadataexportController', ["$scope",'$filter', "MetaDataExport", function($scope, $filter, MetaDataExport) {
 	var $translate = $filter('translate');
 	
+	$scope.progressbarDisplayed = false;
+	
 	$scope.submit=function(){
+		//Show progress bar 
+		$scope.progressbarDisplayed = true;
+		
 		var result = MetaDataExport.get();
 		var fileName = this.file_name;
 		result.$promise.then(function(data) {
 			var file = new Blob([JSON.stringify(data.toJSON())], { type: 'application/json' });
+			//Hide progress bar
+			$scope.progressbarDisplayed = false;
             saveAs(file, fileName + '.json');
     	});
 	};
