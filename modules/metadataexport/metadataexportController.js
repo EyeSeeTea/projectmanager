7dc56fcc778a290d7014c5579c31cddfff75c5d4
Adrian Quintana <adrian.perez@ucl.ac.uk>
2015-01-14 13:21:57 +0000

    anadido funcionalidad para guardar como

--- modules/metadataexport/metadataexportController.js
+++ modules/metadataexport/metadataexportController.js
@@ -1,15 +1,13 @@
 appManagerMSF.controller('metadataexportController', ["$scope",'$filter', "MetaDataExport", function($scope, $filter, MetaDataExport) {
-		var $translate = $filter('translate');
-		
-//		$scope.metadataexport=function(){
-		$scope.submit=function(){
-			console.log($scope);
-			var result=MetaDataExport.get();
-			result.$promise.then(function(data) {
-				var file = new Blob([JSON.stringify(data.toJSON())], { type: 'application/json' });
-	            saveAs(file, 'filename.json');
-	    	});
-		}
-		
+	var $translate = $filter('translate');
+	
+	$scope.submit=function(){
+		var result = MetaDataExport.get();
+		var fileName = this.file_name;
+		result.$promise.then(function(data) {
+			var file = new Blob([JSON.stringify(data.toJSON())], { type: 'application/json' });
+            saveAs(file, fileName + '.json');
+    	});
+	};
 		
 }]);
