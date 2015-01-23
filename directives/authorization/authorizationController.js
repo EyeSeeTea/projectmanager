4c5b566a30627d0842c0fd608b887432b8ac9d01
HelderCastrillon <helder_947@hotmail.com>
2015-01-23 13:20:23 -0500

    Dictectiva de autorización de acceso

---
+++ directives/authorization/authorizationController.js
@@ -0,0 +1,32 @@
+Dhis2Api.directive('d2Authorization', function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'directives/authorization/authorizationView.html',
+		scope: {
+		      menuoption: '@'
+		    }
+	}
+	}); 
+Dhis2Api.controller("d2AuthorizationController", ['$scope',"userAuthorization",'$modal',function ($scope,userAuthorization, $modal) {
+	 console.log($scope.menuoption);
+	var authorization =userAuthorization.get({menuoption:$scope.menuoption});
+	authorization.$promise.then(function(data) {
+		if(data.status=="false")
+			$scope.open();
+	});
+	
+	  $scope.open = function () {
+
+	    var modalInstance = $modal.open({
+	      templateUrl: 'myModalContent.html',
+	      controller: 'ModalInstanceCtrl',
+	      backdrop: false
+	    });
+  };
+}]);
+
+Dhis2Api.controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
+	  $scope.cancel = function () {
+	    $modalInstance.dismiss('cancel');
+	  };
+});
