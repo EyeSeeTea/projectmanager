c9c857b1c24e26d032e9575b835de02100dc8993
Victor Garcia <vgarciabnz@gmail.com>
2016-07-07 12:16:40 +0200

    Minor fixes in DataStore service

--- services/DataStoreService.js
+++ services/DataStoreService.js
@@ -1,6 +1,25 @@
+
+/*
+ Copyright (c) 2015.
+
+ This file is part of Project Manager.
+
+ Project Manager is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Project Manager is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Project Manager.  If not, see <http://www.gnu.org/licenses/>. */
+
 appManagerMSF.factory("DataStoreService", ['DataStore','meUser', '$q', function(DataStore, meUser, $q) {
 
-    var namespace = "project_manager";
+    var namespace = "projectmanager";
     var userid = null;
     var defaultArrayKey = "values";
 
@@ -85,6 +104,7 @@ appManagerMSF.factory("DataStoreService", ['DataStore','meUser', '$q', function(
 
     return {
         getCurrentUserSettings: getCurrentUserSettings,
+        updateCurrentUserSettings: updateCurrentUserSettings,
         getNamespaceKeyValue: getNamespaceKeyValue,
         updateNamespaceKeyArray: updateNamespaceKeyArray
     };
