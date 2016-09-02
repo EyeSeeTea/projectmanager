eb3f3c26b316667b1fd3032ac941d9d62e5bfaae
Victor Garcia <vgarciabnz@gmail.com>
2016-09-02 09:01:44 +0200

    Add containsDigit validator

---
+++ directives/validators/containsDigit.js
@@ -0,0 +1,41 @@
+
+
+/*
+ Copyright (c) 2015.
+
+ This file is part of Project Manager.
+
+ Project Manager is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Project Manager is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Project Manager.  If not, see <http://www.gnu.org/licenses/>. */
+
+
+Dhis2Api.directive('containsDigit', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$validators.containsDigit = function(modelValue, viewValue) {
+                if (ctrl.$isEmpty(modelValue)) {
+                    return true;
+                }
+
+                for (var i = 0; i < viewValue.length; i++) {
+                    if (/\d/.test(viewValue[i])) {
+                        return true;
+                    }
+                }
+                
+                return false;
+            };
+        }
+    };
+});
