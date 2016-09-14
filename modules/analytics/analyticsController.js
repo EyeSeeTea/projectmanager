4916b0c50bd42f110897c530ded1d69926f319be
Victor Garcia <vgarciabnz@gmail.com>
2016-09-14 16:56:00 +0200

    Analytics: add Demographics and use new AnalyticsService

--- modules/analytics/analyticsController.js
+++ modules/analytics/analyticsController.js
@@ -18,36 +18,29 @@
    along with Project Manager.  If not, see <http://www.gnu.org/licenses/>. */
 
 
-appManagerMSF.controller('analyticsController', ["$scope",'$interval', '$filter', "commonvariable", "Analytics", "DataMart", function($scope, $interval, $filter, commonvariable, Analytics, DataMart) {
-	var $translate = $filter('translate');
-	
+appManagerMSF.controller('analyticsController', ["$scope", "AnalyticsService", "DemographicsService", function($scope, AnalyticsService, DemographicsService) {
+
 	$scope.progressbarDisplayed = false;
-	
-	$scope.analytics=function(){
-		Analytics.post();
-		
+
+	$scope.analytics = function () {
+
 		$scope.progressbarDisplayed = true;
 		$scope.summaryDisplayed = true;
-		
-		var inputParameters = {};
-		var previousMessage = "";
-		checkStatus = $interval(function() {
-			var result = DataMart.query(inputParameters);
-			 result.$promise.then(function(data) {
-	    		console.log(data);
-	    		var dataElement = data[0];
-	    		if (dataElement != undefined){
-		    		inputParameters = {lastId: dataElement.uid};
-		    		if (dataElement.completed == true){
-	    				$interval.cancel(checkStatus);
-	    				$scope.progressbarDisplayed = false;
-	    			}
-		    		if (previousMessage != dataElement.message){
-		    			$('#notificationTable tbody').prepend('<tr><td>' + dataElement.time + '</td><td>' + dataElement.message + '</td></tr>');
-		    			previousMessage = dataElement.message;
-			 		}
-	    		}
-	    	});
-          }, 200);
+
+		DemographicsService.updateDemographicData()
+			.then(AnalyticsService.refreshAnalytics)
+			.then(
+				function (success) {
+					$scope.progressbarDisplayed = false;
+				},
+				function (error) {
+					$scope.progressbarDisplayed = false;
+					// TODO Alert about the error
+				},
+				function (dataElement) {
+					$('#notificationTable tbody').prepend('<tr><td>' + dataElement.time + '</td><td>' + dataElement.message + '</td></tr>');
+				}
+			);
 	};
+	
 }]);
