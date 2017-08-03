74efb6af6186cfee49cc6445ef9e57594e8c29f6
Victor Garcia <vgarciabnz@gmail.com>
2017-08-03 16:40:37 +0200

    Removed unused import

--- services/system/SystemService.ts
+++ services/system/SystemService.ts
@@ -17,7 +17,6 @@
  You should have received a copy of the GNU General Public License
  along with Project Manager.  If not, see <http://www.gnu.org/licenses/>. */
 
-import * as angular from 'angular';
 import { Orgunit, Program } from '../../model/model';
 
 export class SystemService {
