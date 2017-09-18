0cb2a34936fe316e73b62cd6fcd958ae971263d9
Victor Garcia <vgarciabnz@gmail.com>
2017-09-18 13:47:19 +0200

    Add AbstractDataStore and apply to one example

---
+++ services/data-store/AbstractDataStoreService.ts
@@ -0,0 +1,58 @@
+
+/*
+ Copyright (c) 2015.
+
+ This file is part of Project Manager.
+
+ Project Manager is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Project Manager is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Project Manager.  If not, see <http://www.gnu.org/licenses/>. */
+
+import { DataStoreNames } from '../services.module'
+
+export abstract class AbstractDataStoreService {
+
+    protected abstract namespace: string;
+
+    static $inject = ['DataStore', 'DataStoreNames'];
+
+    constructor(
+        protected DataStore,
+        protected DataStoreNames: DataStoreNames
+    ){}
+
+    setKeyValue(key, value) {
+        return this.getKeyValue(key)
+            .then( currentValue => {
+                if (currentValue != undefined) {
+                    return this.DataStore.put({ namespace: this.namespace, key: key }, value);
+                } else {
+                    return this.DataStore.save({ namespace: this.namespace, key: key }, value);
+                }
+            })
+    }
+
+    getKeyValue(key) {
+        return this.DataStore.get({ namespace: this.namespace, key: key }).$promise.then(
+            data => data,
+            error => undefined
+        );
+    }
+
+    getKeys() {
+        return this.DataStore.query({ namespace: this.namespace, key: "" }).$promise.then(
+            data => data,
+            error => undefined
+        )
+    }
+
+}
