db12a9f76abbf9f4692e8610da000e378ef81dcb
Victor Garcia <vgarciabnz@gmail.com>
2018-02-01 18:27:50 +0100

    Query _periodstructure table in a single api call

--- features/availabledata/availabledataController.ts
+++ features/availabledata/availabledataController.ts
@@ -86,58 +86,33 @@ export class AvailableData {
 
 	showValidationDatastore() {
 
-		if (this.datastoredRead == false && this.isOnline) {
-			return this.ValidationService.fillDatastore().then(()=>{
-			return this.ValidationService.readDatastore().then(
-				data => {
+		if (this.datastoredRead == false) {
+            const _periodStructurePromise = this.SqlService.executeSqlCode("SELECT iso, monthly, quarterly FROM _periodstructure");
+			return this.$q.all([this.ValidationService.fillDatastore(), _periodStructurePromise]).then( response  => {
+                
+                const _periodStructure = this.formatPeriodStructure(response[1].rows);
+			    return this.ValidationService.readDatastore().then( data => {
+                    angular.forEach(data.datasets, dataset => {
 
-					angular.forEach(data.datasets, dataset => {
+                        if (!(this.valuesDatastore[dataset.missionId] instanceof Array)) { this.valuesDatastore[dataset.missionId] = [] }
+                        if (!(this.valuesDatastore[dataset.siteId]! instanceof Array)) { this.valuesDatastore[dataset.siteId] = [] }
+                        if (!(this.valuesDatastore[dataset.project]! instanceof Array)) { this.valuesDatastore[dataset.project] = [] }
+                        if (!(this.valuesDatastore[dataset.service]! instanceof Array)) { this.valuesDatastore[dataset.service] = [] }
+                        if (!(this.valuesDatastore['zOyMxdCLXBM']! instanceof Array)) { this.valuesDatastore['zOyMxdCLXBM'] = [] }
+                        if (!(this.valuesDatastore['G7g4TvbjFlX']! instanceof Array)) { this.valuesDatastore['G7g4TvbjFlX'] = [] }
 
-						if (!(this.valuesDatastore[dataset.missionId] instanceof Array)) { this.valuesDatastore[dataset.missionId] = [] }
-						if (!(this.valuesDatastore[dataset.siteId]! instanceof Array)) { this.valuesDatastore[dataset.siteId] = [] }
-						if (!(this.valuesDatastore[dataset.project]! instanceof Array)) { this.valuesDatastore[dataset.project] = [] }
-						if (!(this.valuesDatastore[dataset.service]! instanceof Array)) { this.valuesDatastore[dataset.service] = [] }
-						if (!(this.valuesDatastore['zOyMxdCLXBM']! instanceof Array)) { this.valuesDatastore['zOyMxdCLXBM'] = [] }
-						if (!(this.valuesDatastore['G7g4TvbjFlX']! instanceof Array)) { this.valuesDatastore['G7g4TvbjFlX'] = [] }
+                        const _periodRow = _periodStructure[dataset.period];
 
-						this.SqlService.executeSqlCode("SELECT distinct(monthly) FROM _dateperiodstructure WHERE quarterly='" + dataset.period + "' OR weekly='" + dataset.period + "'")
-						
-						.then(data => {
-							
-							angular.forEach(data.rows, row => {
-
-								if (row != undefined) {
-
-									this.fillValuesDatastore(dataset, row,"months");
-
-
-								}
-							});
-					});
-					this.SqlService.executeSqlCode("SELECT distinct(quarterly) FROM _dateperiodstructure WHERE monthly='" + dataset.period + "' OR weekly='" + dataset.period + "'")
-						
-						.then(data => {
-							angular.forEach(data.rows, row => {
-
-								if (row != undefined) {
-
-									this.fillValuesDatastore(dataset, row,"months");
-
-
-								}
-							});
-					});
-
-
-
-						this.fillValuesDatastore(dataset, dataset.period,"");
-
-
-					});
-					this.datastoredRead = true;
-				}
-			
-			)
+                        this.fillValuesDatastore(dataset, dataset.period, "");
+                        if (_periodRow.monthly) {
+                            this.fillValuesDatastore(dataset, _periodRow.monthly, "months");
+                        }
+                        if (_periodRow.quarterly) {
+                            this.fillValuesDatastore(dataset, _periodRow.quarterly, "quarters");
+                        }
+                    });
+                    this.datastoredRead = true;
+				})
 			})
 		}
 	}
@@ -153,7 +128,26 @@ export class AvailableData {
 		this.valuesDatastore['zOyMxdCLXBM']["'" + period + "'"] = true;
 		this.valuesDatastore['G7g4TvbjFlX']["'" + period + "'"] = true;
 
-	}
+    }
+    
+    /**
+    * This method expects an array of objects arrays like
+    * [
+    *    [iso, monthly, quarterly],
+    *    ["2017W02", "201701", "2017Q1"],
+    * ]
+    *
+    */ 
+    formatPeriodStructure (rows: string[][]) {
+        let _str = {};
+        rows.forEach( period => {
+            _str[period[0]] = {
+                monthly: period[1],
+                quarterly: period[2]
+            }
+        });
+        return _str;
+    }
 
 /*
 	fillValuesDatastore_months(dataset, period) {
